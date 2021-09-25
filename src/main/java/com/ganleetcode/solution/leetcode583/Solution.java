b02efc557966287eb9f26c1cee747906f9ad23ae
liyanjie <jackelyj@gmail.com>
2021-09-25 15:38:01 +0800

    update

---
+++ src/main/java/com/ganleetcode/solution/leetcode583/Solution.java
@@ -0,0 +1,30 @@
+package com.ganleetcode.solution.leetcode583;
+
+/**
+ * todo
+ *
+ * @author <a href="dreamerlyj@gmail.com">liyanjie</a>
+ * @since
+ */
+public class Solution
+{
+    public int minDistance(String word1, String word2)
+    {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i < m + 1; i++) {
+            char c1 = word1.charAt(i - 1);
+            for (int j = 1; j < n + 1; j++) {
+                char c2 = word2.charAt(j - 1);
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return m + n - 2 * dp[m][n];
+    }
+
+}
